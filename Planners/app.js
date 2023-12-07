8b38bf24cfd1a905f5e8411a41d2eedcee193f7b
Abhay Thanak <abhaythanak@gmail.com>
2023-12-07 17:33:40 +0530

    1.GSAP using cursor or smooth cursor effect added

--- Planners/app.js
+++ Planners/app.js
@@ -1,7 +1,28 @@
-let page1Content = document.querySelector("#page1-content");
-let Cursor = document.querySelector("#cursor");
+function CursorEffect (){
+    let page1Content = document.querySelector("#page1-content");
+    let Cursor = document.querySelector("#cursor");
 
+     // page1Content.addEventListener("mousemove", function(e){
+     //     Cursor.style.left = e.x+"px";
+     //     Cursor.style.top = e.y+"px";
+// })
 page1Content.addEventListener("mousemove", function(e){
-    Cursor.style.left = e.x+"px";
-    Cursor.style.top = e.y+"px";
+    gsap.to(Cursor,{
+        x:e.x,
+        y:e.y
+    })
 })
+page1Content.addEventListener("mouseenter", function(e){
+    gsap.to(Cursor,{
+        scale:1,
+        opacity:1
+    })
+})
+page1Content.addEventListener("mouseleave", function(e){
+    gsap.to(Cursor,{
+        scale:0,
+        opacity:0
+    })
+})
+}
+CursorEffect();
